6e6f470138e95ebfcdf56ad69054302927e72fa3
Nickolaj <nickolaj99@outlook.com>
2020-11-05 19:22:28 +0100

    added coments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+var hideTemperature = document.getElementById("temperature-value");
+hideTemperature.style.display = "none";
+var hideLocation = document.getElementById("notifikationer");
+hideLocation.style.display = "none";
+var  hideWeatherIcon = document.getElementById("weather-icon")
+hideWeatherIcon.style.display = "none";
 
-	//Funktion som laver min Kelvin om til Celsius
-	var hideTemperature = document.getElementById("temperature-value");
-	hideTemperature.style.display = "none";
-	var hideLocation = document.getElementById("notifikationer");
-	hideLocation.style.display = "none";
-	var  hideWeatherIcon = document.getElementById("weather-icon")
-	hideWeatherIcon.style.display = "none";
 
+//Funktion som laver min Kelvin om til Celsius
 function convertKelvinToCelsius(kelvin) {
 	if (kelvin < (0)) {
 		return 'below absolute zero (0 K)'
@@ -15,24 +15,26 @@ function convertKelvinToCelsius(kelvin) {
 	}
 }
 
+
 function getCity(city){
     var ajaxRequest = new XMLHttpRequest();
     ajaxRequest.onreadystatechange = function(){
 
-        if(ajaxRequest.readyState == 4){
+        if(ajaxRequest.readyState == 4){ // 4 betyder at anmodningen er færdig, og svaret er klar
             //the request is completed, now check its status
-            if(ajaxRequest.status == 200){
+            if(ajaxRequest.status == 200){ // 200 = OK
               //turn JSON into object
               var jsonObj = JSON.parse(ajaxRequest.responseText);
 							showIcon(jsonObj.weather[0].icon);
-              // console.log(jsonObj.weather[0].main);
-              // console.log(jsonObj.weather[0].description);
+
+
 			  //Temperatur-value som nu viser temperaturen i celsius
 			  var element = document.getElementById("temperature-value");
 			  document.getElementById("temperature-value").style.fontSize = "35px";
 			  document.getElementById("temperature-value").style.fontWeight = "800";
 			  document.getElementById("temperature-value").style.display = "block";
-			  element.innerHTML = Math.round(convertKelvinToCelsius(jsonObj.main.temp).toFixed(2)) + " C\u00B0";
+			  element.innerHTML = Math.round(convertKelvinToCelsius(jsonObj.main.temp).toFixed(2)) + " \u00B0C";
+
 			  //sys-country siger hvilket land byen er i så fx DK, SE, GB, NO osv
 			  var element = document.getElementById("notifikationer");
 			  document.getElementById("notifikationer").style.fontSize = "45px";
@@ -42,8 +44,6 @@ function getCity(city){
             }
         }
 	}
-
-
 
 
     x = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=547d9d614973c7dc888cd7cc61b23c04'
@@ -57,33 +57,28 @@ function getCity(city){
 var EnterKey = document.getElementById("input");
 var click = document.getElementById("btn");
 
-function makeVisible(){
-	document.getElementById('city').style.visibility = 'visible';
-	document.getElementById('getWeather').style.visibility = 'visible';
-	document.getElementById('SearchVisibility').style.visibility = 'collapse';
-}
 
+//Viser vejr ikonet på siden
 function showIcon(icon) {
 	hideWeatherIcon.style.display = "block";
   hideWeatherIcon.firstChild.src = 'https://openweathermap.org/img/wn/'+ icon + '@2x.png';
 };
 
 
-click.addEventListener("click", function () {
+click.addEventListener("click", function() {
   console.log(EnterKey.value);
 	getCity(EnterKey.value);
 } );
 
+
+//Giver mulighed for at bruge enter
 EnterKey.addEventListener("keypress", function() {
   sendURL(event.key);
 })
 
+
 function sendURL (enter) {
 	if(event.key=="Enter"){
     getCity(EnterKey.value);
-
-	}
-	else {
-
 	}
 }
